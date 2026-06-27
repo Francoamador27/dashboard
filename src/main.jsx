38f44b685f09edc0e6ccf25bfeb32a2d3f2fb0be
Francoamador27 <francohugoamador@hotmail.com>
2026-06-27 05:35:23 -0300

    actualizacion error

--- src/main.jsx
+++ src/main.jsx
@@ -10,14 +10,8 @@ import "@fontsource/inter/400.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
 
-import GTMBody from "./components/BodyVerification/GTMBody";
-
 createRoot(document.getElementById("root")).render(
     <Provider>
-      {/* GTM / scripts globales */}
-      <GTMBody gtmId={"sadas"} />
-
-      {/* App con router */}
       <RouterProvider router={router} />
     </Provider>
 );
